e889316bd2bc8efb098b7030c996e0827e13ba3f
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-23 14:27:53 +0800

    Update diy-part6.sh

--- diy-part6.sh
+++ diy-part6.sh
@@ -3,7 +3,7 @@
 
 # hlk7628dna dts
 mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7628an_hilink_hlk-7628dna.dts" "target/linux/ramips/dts/mt7628an_hilink_hlk-7628dna.dts"
+cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7628an_hilink_hlk-7628d.dts" "target/linux/ramips/dts/mt7628an_hilink_hlk-7628d.dts"
 
 # hlk7628dna mk
 mkdir -p target/linux/ramips/image/

bd3c080a9691fb8d34067a708347a444b451c5f8
xuxin1955 <x2874836915@qq.com>
2026-05-14 15:37:17 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,10 +7,6 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # Modify default theme
 sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# temp
-git clone https://github.com/xuxin1955/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone https://github.com/xuxin1955/luci-app-temp-status package/luci-app-temp-status
-
 # DbusSmsForwardCPlus
 git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
 
